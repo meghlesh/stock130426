60db103b1206c75902644dfc04babc7cfbd0dcab
meghlesh <meghachaudhari24@gmail.com>
2026-04-13 17:44:17 +0530

    Update settings.py

--- SMS/SMS/settings.py
+++ SMS/SMS/settings.py
@@ -153,6 +153,7 @@ STATICFILES_DIRS = [
 
 STATIC_ROOT = BASE_DIR / "staticfiles"
 
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 
 # Default primary key field type

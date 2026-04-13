835592946ec5ecaaee04fc94c6af5f9df047ed39
meghlesh <meghachaudhari24@gmail.com>
2026-04-13 16:11:57 +0530

    Update settings.py

--- SMS/SMS/settings.py
+++ SMS/SMS/settings.py
@@ -29,13 +29,15 @@ DEBUG = True
 ALLOWED_HOSTS = [
     "api-sms-be-cddgg8d5euh3ckhg.southindia-01.azurewebsites.net",
     "cwsinventory.in",
-    "www.cwsinventory.in"
+    "www.cwsinventory.in",
+     "green-dune-0ff832000.7.azurestaticapps.net"
 ]
 
 CSRF_TRUSTED_ORIGINS = [
     "https://api-sms-be-cddgg8d5euh3ckhg.southindia-01.azurewebsites.net",
     "https://cwsinventory.in",
-    "https://www.cwsinventory.in"
+    "https://www.cwsinventory.in",
+    "https://green-dune-0ff832000.7.azurestaticapps.net"
 ]
  
 #ALLOWED_HOSTS = [ ]

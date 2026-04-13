493b7a01f01de65906530eeba414522ad67cb0b7
meghlesh <meghachaudhari24@gmail.com>
2026-04-13 17:06:45 +0530

    Update urls.py

--- SMS/SMS/urls.py
+++ SMS/SMS/urls.py
@@ -3,7 +3,7 @@ from django.urls import path, include
 from django.views.generic import TemplateView
 
 urlpatterns = [
-    path("", TemplateView.as_view(template_name="accounts/landing.html"), name="index"),
+    path("", TemplateView.as_view(template_name="accounts/index.html"), name="index"),
     path('django-admin/', admin.site.urls),
     path('', include('accounts.urls')),
     path('company/', include('company.urls')),

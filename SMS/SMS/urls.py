da9eb4996c56d3f7a6c417feca26226003b7451e
meghachaudhari24@gmail.com <meghachaudhari24@gmail.com>
2026-04-13 14:33:27 +0530

    first commit

---
+++ SMS/SMS/urls.py
@@ -0,0 +1,11 @@
+from django.contrib import admin
+from django.urls import path, include
+from django.views.generic import TemplateView
+
+urlpatterns = [
+    path("", TemplateView.as_view(template_name="accounts/landing.html"), name="landing"),
+    path('django-admin/', admin.site.urls),
+    path('', include('accounts.urls')),
+    path('company/', include('company.urls')),
+    path("inventory/", include("inventory.urls")),
+]
